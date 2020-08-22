011bc4a16202bb1a3b37363278973018c10f9a64
root <root@localhost.localdomain>
2020-08-22 15:58:44 +0000

    cookies

--- components/Cookies.js
+++ components/Cookies.js
@@ -1,4 +1,8 @@
+import Link from "next/link";
+
 import { useState, useEffect } from "react";
+
+import "../styles/components/Cookies.scss";
 
 export default function Cookies() {
   const [hide, setHide] = useState(true);
@@ -7,12 +11,24 @@ export default function Cookies() {
     let cookies = {};
     document.cookie.split("; ").forEach((cookie) => {
       const parsedCookie = cookie.split("=");
-      cookies[parsedCookie[0]] = parsedCookie[1]
+      cookies[parsedCookie[0]] = parsedCookie[1];
     });
     if (!cookies.cookies_accepted) {
-        setTimeout(() => setHide(false), 2000)
+      setTimeout(() => setHide(false), 2000);
     }
   }, [hide]);
 
-  return hide ? null : <div id="Cookies">Cookies</div>;
+  return hide ? null : (
+    <div id="Cookies">
+      <p>Ta strona uzywa pliki cookies!</p>
+      <p>
+        Uzywajac naszych serwisow godzisz sie na przetwarzanie danych osobowych
+        wedlug naszej{" "}
+        <Link href={"/privacy-policy"}>
+          <a>polityki prywatnosci</a>
+        </Link>{" "}
+        zgodnie z przepisami z ustawy RODO cos tam cos tam bylo
+      </p>
+    </div>
+  );
 }
