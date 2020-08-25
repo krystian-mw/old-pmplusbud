2e4a754c6af7d01cce05085934bdd6f72d418e23
krystian-mw <krystian@mwebdev.co.uk>
2020-08-25 15:25:17 +0000

    Document Fix

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pl">
-        <DocumentHead />
+        <Head />
         <body>
           <Main />
           <NextScript />
